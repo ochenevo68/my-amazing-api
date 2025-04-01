b56f0a6ef372de64a5550ded7bd101ac62387ada
Ranzal <ranzal@pod-1337.localhost>
2025-04-01 13:37:54 +0200

    Added retry mechanic when dealing with external API:
    - Added tenacity lib
    - Retry when failing to get data from restcountries.com

--- src/external/restcountries.py
+++ src/external/restcountries.py
@@ -1,6 +1,7 @@
 from typing import Any
 
 import aiohttp
+from tenacity import retry, stop_after_attempt, wait_fixed
 from typing_extensions import override
 
 from external import DataSource
@@ -10,9 +11,16 @@ class RestcountriesDataSource(DataSource):
     """DataSource for the online restcountries.com API."""
 
     @staticmethod
+    @retry(
+        stop=stop_after_attempt(3),
+        wait=wait_fixed(3),
+        reraise=True,
+    )
     async def _get_async(session: aiohttp.ClientSession, url: str) -> Any:
         async with session.get(url) as response:
-            return await response.json()
+            if response.status == 200:
+                return await response.json()
+        raise TimeoutError()
 
     @override
     async def get_data(self) -> Any:
